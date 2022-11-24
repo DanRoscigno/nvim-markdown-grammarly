e46ff23236eab61270a333d75379b176b7bd91f4
DanRoscigno <dan@roscigno.com>
2022-11-23 23:07:53 -0500

    single sign column

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,9 +1,10 @@
 -- General Settings
 
 vim.o.number       = true
+vim.g.mapleader    = ' '
 vim.o.encoding     = 'utf-8'
 vim.o.fileencoding = 'utf-8'
-vim.o.signcolumn   = 'yes'
+vim.o.signcolumn   = 'yes:1'
 vim.o.tabstop      = 4
 vim.o.shiftwidth   = 4
 vim.o.expandtab    = true
