588c2e482d007922ef3e3d41b66abd4924c54f8d
DanRoscigno <dan@roscigno.com>
2022-11-23 20:44:43 -0500

    remove unused option

--- lua/grammarly.lua
+++ lua/grammarly.lua
@@ -1,7 +1,5 @@
 
 require('lspconfig').grammarly.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
     cmd = { "/home/droscigno/GitHub/grammarly/extension/node_modules/.bin/grammarly-languageserver", "--stdio" },
     filetypes = { "markdown", "text" },
     init_options = {
