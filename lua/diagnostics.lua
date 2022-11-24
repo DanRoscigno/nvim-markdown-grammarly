79b36b8298adee5e6277980fd66d57454610984a
DanRoscigno <dan@roscigno.com>
2022-11-23 20:04:06 -0500

    minimal

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,5 +1,6 @@
 
-local config = {
+vim.diagnostic.config(
+ {
   -- disable virtual text
   virtual_text = false,
   -- show signs
@@ -17,28 +18,10 @@ local config = {
     header = "",
     --prefix = "",
   },
-}
+})
 
-vim.diagnostic.config(config)
 
-local silent_noremap = {
-    noremap = true,
-    silent = true
-}
-
-local function nsnoremap(lhs, rhs)
-    vim.api.nvim_set_keymap('n', lhs, rhs, silent_noremap)
-end
-
-nsnoremap(']d',   '<CMD>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>')
-nsnoremap('[d',   '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>')
-nsnoremap('gl',   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>')
-nsnoremap('gD',   '<cmd>lua vim.lsp.buf.declaration()<CR>')
-nsnoremap('gd',   '<cmd>lua vim.lsp.buf.definition()<CR>')
-nsnoremap('K',    '<cmd>lua vim.lsp.buf.hover()<CR>')
-nsnoremap('gi',   '<cmd>lua vim.lsp.buf.implementation()<CR>')
-nsnoremap('gr',   '<cmd>lua vim.lsp.buf.references()<CR>')
-nsnoremap('C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-nsnoremap('<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
+vim.api.nvim_set_keymap('n', ']d',   '<CMD>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[d',   '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', { noremap = true, silent = true })
 
 
