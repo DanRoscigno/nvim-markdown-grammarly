08009d80b6377f5e8d822b9c18145471e3b0ca32
DanRoscigno <dan@roscigno.com>
2022-11-23 20:37:59 -0500

    add notes

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,12 +1,10 @@
 
 vim.diagnostic.config(
  {
-  -- disable virtual text
+  -- Grammarly default is to show the issue
+  -- at the end of the line, I prefer it in
+  -- a float, so turn off the virtual text
   virtual_text = false,
-  -- show signs
-  signs = {
-    active = signs,
-  },
   update_in_insert = true,
   underline = true,
   severity_sort = true,
@@ -16,12 +14,24 @@ vim.diagnostic.config(
     border = "rounded",
     source = "always",
     header = "",
-    --prefix = "",
   },
 })
 
-
-vim.api.nvim_set_keymap('n', ']d',   '<CMD>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '[d',   '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', { noremap = true, silent = true })
-
+-- If there are any diagnostic messages from the Marksman 
+-- or Grammarly LSPs, then there will be marks in the far left
+-- column.  `E` for error, `W` for warning, etc.  The two
+-- keymaps below will cycle through the diagnostic messages.
+-- type `]d` to go forward, and `[d` to go backward:
+vim.api.nvim_set_keymap(
+  'n',
+  ']d',
+  '<CMD>lua vim.diagnostic.goto_next()<CR>',
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  'n',
+  '[d',
+  '<cmd>lua vim.diagnostic.goto_prev()<CR>',
+  { noremap = true, silent = true }
+)
 
